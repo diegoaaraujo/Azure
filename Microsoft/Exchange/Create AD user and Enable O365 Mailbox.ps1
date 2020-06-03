ce8668ac43705fba19624def46c24b557e1a2aad
Diego Araujo | MOQdigital <diego_aaraujo@yahoo.com>
2020-06-04 09:41:41 +1000

    added test1.txt

---
+++ Microsoft/Exchange/Create AD user and Enable O365 Mailbox.ps1
@@ -0,0 +1,34 @@
+  
+#add exchange snap-in
+Add-PSSnapin Microsoft.Exchange.Management.PowerShell.SnapIn;
+
+#get username with input
+"";
+"----- by Flo Faber -----";
+"";
+
+
+#get first and lastname with input
+$firstname = Read-Host "Vorname";
+$lastname = Read-Host "Nachname";
+
+$username = $firstname + "." + $lastname;
+$username_domain = $username + '@faber.int';
+
+$name = $firstname + $lastname;
+$displayname = $firstname + " " + $lastname;
+
+#get password with input
+$password = Read-Host "Passwort vergeben" -AsSecureString;
+
+#get Organisation Unit with input
+#$ou_input = Read-Host "Organisation Unit";
+#$ou = "OU=$ou_input,DC=AD,DC=faber,DC=int,DC=local";
+
+#Add AD-User and Mailbox
+New-ADUser -Name $displayname -GivenName $firstname -Surname $lastname -SamAccountName $username -UserPrincipalName $username_domain -AccountPassword $password -PassThru | Enable-ADAccount;
+
+#Enable created mailbox
+Enable-Mailbox -Identity $username;
+
+cmd /c pause | out-null
